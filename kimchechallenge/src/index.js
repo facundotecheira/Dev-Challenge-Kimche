ddde09426e6bb9c76e42f2584840c9f66ccc005d
facundotecheira <facundotecheira00@gmail.com>
2022-05-15 19:45:47 -0300

    Proyecto finalizado

---
+++ kimchechallenge/src/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
+
+
+ReactDOM.render(<ApolloProvider client={client}>
+  <App />
+</ApolloProvider>, document.getElementById('root'));
